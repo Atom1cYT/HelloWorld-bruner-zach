d217c5ddd8674eb7a4218bc05ea2eec4ee94f250
Atom1cYT <146125539+Atom1cYT@users.noreply.github.com>
2024-02-16 09:21:20 -0600

    Initial commit

---
+++ HelloWorld bruner zach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld_bruner_zach
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your first Name?"); // Ask the user for first name
+            string firstname = Console.ReadLine();         // Assigns user input to firstname
+            Console.WriteLine("What is your last name?");  // Assigns the user last name
+            string lastname = Console.ReadLine();          // Assigns the user input to lastname variable
+
+            // 
+            Console.WriteLine("Hello, " + firstname + " " +lastname + "!"); // Prints the user input 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld-bruner-zach
+ 
